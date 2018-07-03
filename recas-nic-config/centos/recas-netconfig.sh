b50f3760870f5dac39965616e994fa2ffa2c1d42
mtangaro <ma.tangaro@gmail.com>
2018-07-03 10:22:18 +0000

    init centos config

---
+++ recas-nic-config/centos/recas-netconfig.sh
@@ -0,0 +1,153 @@
+#!/bin/bash
+
+# Log file
+LOGFILE='/var/log/recas-netconfig.log'
+
+#________________________________
+# Check for private IP
+function find_private_nic(){
+
+  # If the private interface is on use this command to determine its name
+  PRIVATE_IFACE_NAME=$(ip a | grep -E '172.30.[0-9]{1,3}' -B 2 | head -1 | awk -F\: '{print $2}' | sed "s/^ //")
+
+  # Exit if the variable is unset or empty
+  if [ -z "$PRIVATE_IFACE_NAME" ]
+  then
+    echo "The variable PRIVATE_IFACE_NAME is not set" >> $LOGFILE
+    exit 1
+  fi
+
+  # Determine the private IP assigned by OpenStack's DHCP
+  PRIVATE_IP=$(hostname -I | grep -Ewo '172.30.[0-9]{1,3}.[0-9]{1,3}')
+
+  # Exit if the variable is unset or empty
+  if [ -z "$PRIVATE_IP" ]
+  then
+    echo "The variable PRIVATE_IP is not set" >> $LOGFILE
+    exit 3
+  fi
+
+  # Configure the private IP as static
+  PRIVATE_IFCFC_FILE=/etc/sysconfig/network-scripts/ifcfg-$PRIVATE_IFACE_NAME
+
+  true
+
+}
+
+#________________________________
+# Check for public IP
+function find_public_nic(){
+
+  # If the public interface is on use this command to determine its name
+  PUBLIC_IFACE_NAME=$(ip a | grep -E '90.147.[0-9]{1,3}' -B 2 | head -1 | awk -F\: '{print $2}' | sed "s/^ //")
+
+  # Exit if the variable is unset or empty
+  if [ -z "$PUBLIC_IFACE_NAME" ]
+  then
+    echo "The variable PUBLIC_IFACE_NAME is not set" >> $LOGFILE
+    exit 2
+  fi
+
+  # Determine the public IP assigned by OpenStack's DHCP
+  PUBLIC_IP=$(hostname -I | grep -Ewo '90.147.[0-9]{1,3}.[0-9]{1,3}')
+
+  # Exit if the variable is unset or empty
+  if [ -z "$PUBLIC_IP" ]
+  then
+    echo "The variable PUBLIC_IP is not set" >> $LOGFILE
+    exit 4
+  fi
+
+  # Configure the public IP as static
+  PUBLIC_IFCFC_FILE=/etc/sysconfig/network-scripts/ifcfg-$PUBLIC_IFACE_NAME
+
+  true
+
+}
+
+#________________________________
+#________________________________
+# Main script
+
+# Check if the number of network interfaces is not two and decide how to behave
+if [[ $(ls -A /sys/class/net | wc -l)  == 3 ]]
+then
+
+  # Wait a little for DHCP lease
+  sleep 15
+
+  # Call both here to set all needed variables
+  find_private_nic
+  find_public_nic
+
+  # Configure the private IP as static
+  echo 'BOOTPROTO=static' > $PRIVATE_IFCFC_FILE
+  echo 'DEVICE='$PRIVATE_IFACE_NAME >> $PRIVATE_IFCFC_FILE
+  echo 'ONBOOT=yes' >> $PRIVATE_IFCFC_FILE
+  echo 'TYPE=Ethernet' >> $PRIVATE_IFCFC_FILE
+  echo 'USERCTL=no' >> $PRIVATE_IFCFC_FILE
+  echo 'IPADDR='$PRIVATE_IP >> $PRIVATE_IFCFC_FILE
+  echo 'NETMASK=255.255.255.0' >> $PRIVATE_IFCFC_FILE
+
+  # Configure the public IP as static
+  echo 'BOOTPROTO=dhcp' > $PUBLIC_IFCFC_FILE
+  echo 'DEVICE='$PUBLIC_IFACE_NAME >> $PUBLIC_IFCFC_FILE
+  echo 'ONBOOT=yes' >> $PUBLIC_IFCFC_FILE
+  echo 'TYPE=Ethernet' >> $PUBLIC_IFCFC_FILE
+  echo 'USERCTL=no' >> $PUBLIC_IFCFC_FILE
+
+  # Default gateway to be configured
+  PUBLIC_GW=$(echo $PUBLIC_IP | awk -F\. '{print $1"."$2"."$3".1"}')
+
+  # Exit if the variable is unset or empty
+  if [ -z "$PUBLIC_GW" ]
+  then
+    echo "The variable PUBLIC_GW is not set" >> $LOGFILE
+    exit 5
+  fi
+
+  # Current default gateway configuration
+  DEFAULT_GW_CONF=$(ip r | grep default)
+
+  # Exit if the variable is unset or empty
+  if [ -z "$DEFAULT_GW_CONF" ]
+  then
+    echo "The variable DEFAULT_GW_CONF is not set" >> $LOGFILE
+    exit 6
+  fi
+
+  # Configure the new default gateway
+  ip r delete $DEFAULT_GW_CONF && ip r add default via $PUBLIC_GW dev $PUBLIC_IFACE_NAME
+
+else
+
+  # If only one interface is found, we determine its name and discharge the other one.
+
+  # If the interface is on use this command to determine its name
+  IFACE_NAME=$(ip a | grep -E '172.30.[0-9]{1,3}|90.147.[0-9]{1,3}' -B 2 | head -1 | awk -F\: '{print $2}' | sed "s/^ //")
+
+  # Exit if the variable is unset or empty
+  if [ -z "$IFACE_NAME" ]
+  then
+    echo "The variable IFACE_NAME is not set" >> $LOGFILE
+    exit 7
+  fi
+
+  # Configure the IP as dhcp
+  IFCFC_FILE=/etc/sysconfig/network-scripts/ifcfg-$IFACE_NAME
+
+  # Configure the private nic as dhcp
+  echo 'BOOTPROTO=dhcp' > $IFCFC_FILE
+  echo 'DEVICE='$IFACE_NAME >> $IFCFC_FILE
+  echo 'ONBOOT=yes' >> $IFCFC_FILE
+  echo 'TYPE=Ethernet' >> $IFCFC_FILE
+  echo 'USERCTL=no' >> $IFCFC_FILE
+
+  # Remove the remaining unuseful configuration file
+  if [[ $IFACE_NAME == 'eth0' ]]; then
+    rm /etc/sysconfig/network-scripts/ifcfg-eth1
+  elif [[ $IFACE_NAME == 'eth1' ]]; then
+    rm /etc/sysconfig/network-scripts/ifcfg-eth0
+  fi
+
+fi # end main if
